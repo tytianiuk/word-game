a5641f25c3c84cb971fa8188a057bbf8a0450b90
Tytianiuk Artem <tytianiuk@gmail.com>
2025-12-14 13:36:28 +0200

    fix: change the case of letters on the board

--- src/utils/fenToBoard.ts
+++ src/utils/fenToBoard.ts
@@ -1,7 +1,9 @@
 const fenToBoard = (fen: string): string[][] => {
   return fen
     .split('/')
-    .map((row) => row.split('').map((ch) => (ch === '0' ? '' : ch)));
+    .map((row) =>
+      row.split('').map((ch) => (ch === '0' ? '' : ch.toUpperCase()))
+    );
 };
 
 export default fenToBoard;
